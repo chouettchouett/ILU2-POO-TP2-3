d1d8353b9766c493daceff854ebca5189e04af00
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2023-10-24 11:48:26 +0200

    2-6 aha-isvendeur

--- src/frontiere/BoundaryLibererEtal.java
+++ src/frontiere/BoundaryLibererEtal.java
@@ -10,7 +10,21 @@ public class BoundaryLibererEtal {
 	}
 
 	public void libererEtal(String nomVendeur) {
-		//TODO a completer
+		boolean vendeurReconnu;
+		vendeurReconnu = controlLibererEtal.isVendeur(nomVendeur);
+		if (!vendeurReconnu) {
+			System.out.println("Mais vous n etes pas inscrits sur notre marche aujourd'hui !");
+		} else {
+			String[] donneesEtal = null;
+			boolean etalOccupe;
+			donneesEtal = controlLibererEtal.libererEtal(nomVendeur);
+			etalOccupe = donneesEtal[0].equalsIgnoreCase("true");
+			if (etalOccupe) {
+				System.out.println(
+						"Vous avez vendu " + donneesEtal[4] + " sur " + donneesEtal[3] + " " + donneesEtal[2] + ".\n");
+				System.out.println("Au revoir " + nomVendeur + " , passez une bonne journee.\n");
+			}
+		}
 	}
 
 }
