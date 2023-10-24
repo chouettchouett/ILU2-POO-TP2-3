2eaaaad313ef3e4ac6339c8c288325b5b57d6741
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2023-10-24 11:55:55 +0200

    Update BoundaryAfficherMarche.java

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,22 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche;
+		infosMarche = controlAfficherMarche.donnerInfosMarche();
+		if (infosMarche.length==0) {
+			System.out.println("Le marche est vide, revenez plus tard.");
+		} else {
+			StringBuilder chaine = new StringBuilder();
+			chaine.append(nomAcheteur+", vous trouverez au marche:\n");
+			for (int i = 0; i < infosMarche.length; i++) {
+				chaine.append("   -"+infosMarche[i]);
+				i++;
+				chaine.append(" qui vend "+infosMarche[i]);
+				i++;
+				chaine.append(" "+infosMarche[i]+".\n");
+				
+			}
+			System.out.println(chaine);
+		}
 	}
 }
